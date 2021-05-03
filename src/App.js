ce28d835f0d8115ce1e24d72d10316cbcd6f6c56
ginokin <ginokinh@gmail.com>
2021-05-03 21:17:43 +0900

    カウントボタン作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset = () => setCount(0)
+
   return (
-    <div>
-      This is a template for React
-   </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <button onClick={reset}>reset</button>
+    </>
   );
 }
 
